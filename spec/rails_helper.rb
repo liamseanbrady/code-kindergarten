f127a92f82ed79bac324d04b80e1a87583f27e62
Liam Sean Brady <liamseanbrady@gmail.com>
2015-05-22 00:18:04 +0100

    Modify database cleaner config

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -60,8 +60,4 @@ RSpec.configure do |config|
   config.before(:each, :js => true) do
     DatabaseCleaner.strategy = :truncation
   end
-
-  config.after(:each) do
-    DatabaseCleaner.clean
-  end
 end
