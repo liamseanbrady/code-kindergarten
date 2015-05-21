61ef4b74539e83a7af468d104f9ca866ebbd67d8
Liam Sean Brady <liamseanbrady@gmail.com>
2015-05-22 00:35:30 +0100

    Add default limit for 'opoular' method and interpolate value in SQL

--- app/models/report_card.rb
+++ app/models/report_card.rb
@@ -1,19 +1,20 @@
 class ReportCard < ActiveRecord::Base
   has_many :likes
 
-  def self.popular
-    find_by_sql(popular_report_cards_query)
+  def self.popular(limit = 20)
+    find_by_sql(popular_report_cards_query(limit))
   end
 
   private
 
-  def self.popular_report_cards_query
+  def self.popular_report_cards_query(limit)
     <<-SQL
     SELECT report_cards.*, COUNT(likes.report_card_id) AS like_count FROM report_cards
     LEFT JOIN likes
     ON report_cards.id=likes.report_card_id
     GROUP BY report_cards.id
-    ORDER BY like_count;
+    ORDER BY like_count DESC
+    LIMIT #{limit};
     SQL
   end
 end
