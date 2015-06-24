f4863dbe0b0d9f3db0852513f8347b02cde5b225
Liam Sean Brady <liamseanbrady@gmail.com>
2015-06-24 18:02:57 +0100

    Add more details to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,10 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-report_card = ReportCard.create(title: 'SQL in Ruby', session_length: 100, grade: 'B', description: 'This is awesome, man. Really cool!')
+like = Like.create
+ruby = Category.create(name: 'Ruby')
+
+ReportCard.create(title: 'SQL in Ruby', session_length: 100, grade: 'B', description: 'This is awesome, man. Really cool!', category: ruby)
+liked_report_card = ReportCard.create(title: 'Arrays in Ruby', session_length: 100, grade: 'A', description: 'Did really well here!')
+
+liked_report_card.likes << like
